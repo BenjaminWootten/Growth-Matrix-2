3de2910f1638521acca218a6980b2f82f04ea5b8
BenjaminWootten <bwootten@udel.edu>
2024-02-24 14:22:26 -0500

    began work on level completion system

--- boxes/green/green_box.gd
+++ boxes/green/green_box.gd
@@ -1,8 +1,12 @@
 extends Area3D
 
+@onready var green_box_root = self.get_parent()
+
+func _on_ready():
+	self.on_body_entered.connect(green_box_root._on_box_filled)
+
 func _on_body_entered(body):
 	$MeshInstance3D.mesh.material.albedo_color = Color(0.1, 0.9, 0.6, 1.0)
 
 func _on_body_exited(body):
 	$MeshInstance3D.mesh.material.albedo_color = Color(0.0, 0.55, 0.0, 1.0)
-
--- boxes/green/green_boxes.gd
+++ boxes/green/green_boxes.gd
@@ -1,4 +1,10 @@
 extends Node3D
 
-func on_green_box_filled():
+var total_boxes = self.get_child_count()
+var filled_boxes = 0
+
+func _on_box_filled():
 	print("a")
+	filled_boxes += 1
+	if filled_boxes == total_boxes:
+		print("woohoo")
