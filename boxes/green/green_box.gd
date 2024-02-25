482ec596ebf3927e2b49dae2c2731ccb7abcfb45
BenjaminWootten <bwootten@udel.edu>
2024-02-24 22:19:52 -0500

    level completion is detectable
    green box meshes made unique so their color changes separately

--- boxes/green/green_box.gd
+++ boxes/green/green_box.gd
@@ -1,12 +1,14 @@
 extends Area3D
 
-@onready var green_box_root = self.get_parent()
+@onready var green_box_root = get_parent()
 
-func _on_ready():
-	self.on_body_entered.connect(green_box_root._on_box_filled)
+func _ready():
+	self.body_entered.connect(green_box_root._on_green_box_body_entered)
+	$MeshInstance3D.mesh = $MeshInstance3D.mesh.duplicate()
+	$MeshInstance3D.mesh.material = $MeshInstance3D.mesh.material.duplicate()
 
-func _on_body_entered(body):
-	$MeshInstance3D.mesh.material.albedo_color = Color(0.1, 0.9, 0.6, 1.0)
+func _on_body_entered(_body):
+	$MeshInstance3D.mesh.material.albedo_color = Color(0.5, 0.25, 0.8, 1.0)
 
-func _on_body_exited(body):
+func _on_body_exited(_body):
 	$MeshInstance3D.mesh.material.albedo_color = Color(0.0, 0.55, 0.0, 1.0)
--- boxes/green/green_boxes.gd
+++ boxes/green/green_boxes.gd
@@ -1,10 +1,9 @@
 extends Node3D
 
-var total_boxes = self.get_child_count()
+@onready var total_boxes = self.get_child_count()
 var filled_boxes = 0
 
-func _on_box_filled():
-	print("a")
+func _on_green_box_body_entered(body):
 	filled_boxes += 1
 	if filled_boxes == total_boxes:
-		print("woohoo")
+		print("level complete")
